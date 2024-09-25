a88ad65b9b5b81e0c2f99902e7805b160cbf13a3
danielleond99 <87273355+danielleond99@users.noreply.github.com>
2024-09-25 15:06:10 -0400

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Daniel Leon on 25/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
